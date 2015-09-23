87f5060a3687af0f0503503147ef454b034a0086
mfisher <mac@mediotype.com>
2015-09-23 12:04:12 -0600

    Using relative file paths via __DIR__

--- restoredb.php
+++ restoredb.php
@@ -1,62 +1,61 @@
 #!/usr/bin/php -q
 <?php
 
-/**
- * FEATURES
- *
- * List DB backup files in backups dir
- */
-$projectRoot = "/usr/local/bin/restoreAcademyDB/";
+function writeLog($str)
+{
+    $logFile = __DIR__."/var/log/restore.log";
 
-function writeLog($str) {
-        $logFile = "/usr/local/bin/restoreAcademyDB/var/log/restore.log";
-
-        $write = sprintf("sudo -u root echo %s %s >> %s", date('Y-m-d h:i:s'), $str, $logFile);
-        shell_exec($write);
+    $write = sprintf("sudo -u root echo %s %s >> %s", date('Y-m-d h:i:s'), $str, $logFile);
+    shell_exec($write);
 }
 
-function echoHelp() {
-        echo "\n";
-        echo "Usage: restoredb [-import] [user] [password] [file to import]\n";
-        echo "                 [-list]\n";
-        echo "                 [-help]\n";
-        echo "\n";
-        echo "Import -- Import file from backups stored in backups directory.\n";
-        echo "List   -- List backup files in the backups directory.\n";
-        echo "Help   -- Print help screen.\n";
-        echo "\n";
-        echo "*NOTE*: Backup files must be placed in /usr/local/bin/restoreAcademyDB/backups\n";
-        echo "        Only give the filename, not the full path.\n";
-        echo "\n";
+writeLog("Testing __DIR__ capabilities.");
+
+function echoHelp()
+{
+    echo "\n";
+    echo "Usage: restoredb [-import] [user] [password] [file to import]\n";
+    echo "                 [-list]\n";
+    echo "                 [-help]\n";
+    echo "\n";
+    echo "Import -- Import file from backups stored in backups directory.\n";
+    echo "List   -- List backup files in the backups directory.\n";
+    echo "Help   -- Print help screen.\n";
+    echo "\n";
+    echo "*NOTE*: Backup files must be placed in /usr/local/bin/restoreAcademyDB/backups\n";
+    echo "        Only give the filename, not the full path.\n";
+    echo "\n";
 }
 
-function listBackupsDir() {
-        $files = scandir("/usr/local/bin/restoreAcademyDB/backups");
+function listBackupsDir()
+{
+    $files = scandir("/usr/local/bin/restoreAcademyDB/backups");
 
-        echo "\n";
-        echo "Available Backup Files:\n";
+    echo "\n";
+    echo "Available Backup Files:\n";
 
-        foreach ($files as $file) {
-                $fileParts = pathinfo($file);
+    foreach ($files as $file) {
+        $fileParts = pathinfo($file);
 
-                if ($fileParts['extension'] !== 'sql' || $file !== '.' || $file !== '..') {
-                        echo $file."\n";
-                        echo $fileParts;
-                }
+        if ($fileParts['extension'] !== 'sql' || $file !== '.' || $file !== '..') {
+            echo $file . "\n";
+            echo $fileParts;
         }
+    }
 
-        echo "\n";
+    echo "\n";
 }
+
 
 # make var dir if it doesn't exist
-if (!file_exists($projectRoot."var")) {
-        $makeVar = sprintf("sudo -u root mkdir %s%s", $projectRoot, "var");
+if (!file_exists(__DIR__."/var")) {
+        $makeVar = sprintf("sudo -u root mkdir %s%s", __DIR__, "/var");
         shell_exec($makeVar);
 }
 
 # make log dir inside var if it does not exist
-if (!file_exists($projectRoot."/var/log")) {
-        $makeLog = sprintf("sudo -u root mkdir %s%s", $projectRoot, "var/log");
+if (!file_exists(__DIR__."/var/log")) {
+        $makeLog = sprintf("sudo -u root mkdir %s%s", __DIR__, "/var/log");
         shell_exec($makeLog);
 }
 
@@ -73,4 +72,3 @@ if ($argv[1] === "-help") {
 if ($argv[1] === "-list") {
         listBackupsDir();
 }
-
